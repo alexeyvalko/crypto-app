d872aefb53f8a2b7002c96f253d1a2ad543bee67
Alexey Valko <valko.alexey@gmail.com>
2022-02-12 23:44:40 +0300

    config: set up

---
+++ Client/src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/main.css';
+import { FC } from 'react';
+
+export const App: FC = () => (
+  <div>
+    test
+    test
+  </div>
+);
---
+++ Client/webpack.prod.js
@@ -0,0 +1,54 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                auto: undefined,
+                localIdentName: '[local]--[hash:base64:5]'
+              },
+              sourceMap: true,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+      }),
+      new CssMinimizerPlugin({
+        test: /\.css$/i,
+      }),
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[hash].css',
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+    }),
+  ],
+});
